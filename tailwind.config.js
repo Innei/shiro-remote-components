95cda5b2d6ef6fa22a8ec73012e73fcf99869337
Innei <i@innei.in>
2024-02-11 23:07:13 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // prefix: 'cn-',
-  darkMode: ['class', '[data-mode="dark"]'],
+  darkMode: ['class', 'html[data-mode="dark"]'],
   content: ['src/**/*.{ts,tsx}', './storybook/**/*.{ts,tsx}'],
   corePlugins:
     process.env.BUILD == '1'
